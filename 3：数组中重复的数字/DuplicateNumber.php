c542437b7f0dafa8536bb175ae9f52109261c98b
gaoqinghuang <617103154@qq.com>
2018-05-02 13:51:36 +0800

    [feature]第三题

--- 3：数组中重复的数字/DuplicateNumber.php
+++ 3：数组中重复的数字/DuplicateNumber.php
@@ -4,5 +4,29 @@
 也不知道重复了几次，请找出任意重复的数字。 例如，
 输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出为2或3*/
 
+//直接用php中的函数实现
+/*function duplicateNumber($data)
+{
+    $unique_arr = array_unique($data);
+    return array_diff_assoc($data,$unique_arr);
+}*/
+//foreach循环一次,时间复杂度O(n),此方法更好
+function duplicateNumber($data)
+{
+    $newData = [];
+    $returnDate = [];
+    foreach($data as $key=>$value)
+    {
+        if(isset($newData[$value]))
+        {
+            $returnDate[] = $value;
+        }
+        else
+        {
+            $newData[$value] = $value;
+        }
+    }
+    return  $returnDate;
+}
 $data=array(2,3,1,0,2,5,3);
-print_r(array_unique($data));
+print_r(duplicateNumber($data));
