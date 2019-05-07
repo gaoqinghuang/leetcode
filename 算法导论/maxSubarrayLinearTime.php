e571fe6c3fd8978cb04f0ad4a4637fe7932afd15
gaoqinghuang <gaoqinghuang@xiaoyouzi.com>
2019-05-07 14:03:28 +0800

    寻找最大子字组的和，动态规划

--- 算法导论/maxSubarrayLinearTime.php
+++ 算法导论/maxSubarrayLinearTime.php
@@ -8,61 +8,50 @@ ini_set('display_startup_errors', 'on');
 class help
 {
     /**
-     * 寻找最大子数组的和，分治算法  时间复杂度nlgn
+     * 寻找最大子数组的和，动态规划  时间复杂度n
      * */
     public function execute()
     {
-        $array = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7];
-        $max   = $this->findMaximumSubarray($array, 0, count($array) - 1);
-        dump($max);
+        $array     = [];
+        $array[]   = [1, 2, 3, 4, 5, 6];//21
+        $array[]   = [0, 0, 18, 0, 0, -6];//18
+        $array[]   = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7];//43
+        $array[]   = [-2, 11, -4, 13, -5, -2];//20
+        $array[]   = [-2, 11, -4, 13, -5, -2];//20
+        $relust    = [21, 18, 43, 20, 20];
+        $isSuccess = true;
+        foreach ($array as $key => $arr) {
+            if ($this->maxSubarrayLinearTime($arr) != $relust[$key]) {
+                $isSuccess = false;
+                $errorMes  = "第{$key}个错误";
+            }
+        }
+
+        if (!$isSuccess) {
+            echo $errorMes;
+        } else {
+            echo '成功啦';
+        }
     }
 
     /**
-     * 寻找最大子数组的和
      * @param $array
-     * @param $low
-     * @param $high
      * @return int
      */
-    private function findMaximumSubarray($array, $low, $high): int
+    private function maxSubarrayLinearTime($array)
     {
-        if ($low == $high) {
-            return $array[$low];
-        }
-        $mid = floor(($low + $high) / 2);
-        //要嘛在左
-        $leftMax = $this->findMaximumSubarray($array, $low, $mid);
-        //要嘛在右
-        $rightMax = $this->findMaximumSubarray($array, $mid + 1, $high);
-        //要嘛在中间
-        $midMax = $this->findMaxCrossiongSubarray($array, $low, $mid, $high);
-        return max($leftMax, $rightMax, $midMax);
-    }
+        $max = $now = 0;
+        foreach ($array as $value) {
+            $now = $now + $value;
+            if ($now > $max) {
+                $max = $now;
+            }
+            if ($now <= 0) {
+                $now = 0;
+            }
 
-    /**
-     * 寻找跨越中间值最大子数组的和 时间复杂度n
-     * @param $array
-     * @param $low
-     * @param $mid
-     * @param $high
-     * @return int
-     */
-    private function findMaxCrossiongSubarray($array, $low, $mid, $high): int
-    {
-        //左边
-        $leftMax = $rightMax = $now = 0;
-        for ($i = $mid; $i >= $low; $i--) {
-            $now     += $array[$i];
-            $leftMax = max($now, $leftMax);
-        };
-
-        //右边
-        $now = 0;
-        for ($i = $mid + 1; $i <= $high; $i++) {
-            $now      += $array[$i];
-            $rightMax = max($now, $rightMax);
         }
-        return $leftMax + $rightMax;
+        return $max;
     }
 
 
