6d2d41f709ebf76d0f4358f9d938a32178a33b41
gaoqinghuang <gaoqinghuang@xiaoyouzi.com>
2021-12-27 16:29:33 +0800

    560. 和为 K 的子数组

--- 560. 和为 K 的子数组/main.go
+++ 560. 和为 K 的子数组/main.go
@@ -45,20 +45,6 @@ func subarraySum(nums []int, k int) int {
 	return result
 }
 
-func subarraySum2(nums []int, k int) int {
-	count, pre := 0, 0
-	m := map[int]int{}
-	m[0] = 1
-	for i := 0; i < len(nums); i++ {
-		pre += nums[i]
-		if _, ok := m[pre - k]; ok {
-			count += m[pre - k]
-		}
-		m[pre] += 1
-	}
-	return count
-}
-
 //暴力循环破解
 func subarraySum1(nums []int, k int) int {
 	result := 0
