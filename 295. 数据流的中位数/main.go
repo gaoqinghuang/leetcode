57ebb45c83a1ce8318b640561f4650c12be2ab55
高清煌 <617103154@qq.com>
2022-03-15 21:52:59 +0800

    295. 数据流的中位数 大小根堆

--- 295. 数据流的中位数/main.go
+++ 295. 数据流的中位数/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
@@ -47,26 +48,27 @@ func main() {
 	fmt.Println(m.FindMedian())
 }
 
-type MedianFinder struct {
-	Arr []int
-}
-
-func Constructor() MedianFinder {
-	return MedianFinder{Arr: make([]int, 0)}
-}
-
-func (this *MedianFinder) AddNum(num int) {
-	index := sort.SearchInts(this.Arr, num)
-	this.Arr = append(this.Arr[0:index], append([]int{num}, this.Arr[index:]...)...)
-}
-
-func (this *MedianFinder) FindMedian() float64 {
-	if len(this.Arr)&1 == 1 { //奇数
-		return float64(this.Arr[len(this.Arr)/2])
-	}
-	//偶数
-	return (float64(this.Arr[len(this.Arr)/2]) + float64(this.Arr[len(this.Arr)/2-1])) / 2
-}
+//
+//type MedianFinder struct {
+//	Arr []int
+//}
+//
+//func Constructor() MedianFinder {
+//	return MedianFinder{Arr: make([]int, 0)}
+//}
+//
+//func (this *MedianFinder) AddNum(num int) {
+//	index := sort.SearchInts(this.Arr, num)
+//	this.Arr = append(this.Arr[0:index], append([]int{num}, this.Arr[index:]...)...)
+//}
+//
+//func (this *MedianFinder) FindMedian() float64 {
+//	if len(this.Arr)&1 == 1 { //奇数
+//		return float64(this.Arr[len(this.Arr)/2])
+//	}
+//	//偶数
+//	return (float64(this.Arr[len(this.Arr)/2]) + float64(this.Arr[len(this.Arr)/2-1])) / 2
+//}
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
@@ -75,4 +77,43 @@ func (this *MedianFinder) FindMedian() float64 {
  * param_2 := obj.FindMedian();
  */
 
+type MedianFinder struct {
+	queMin, queMax hp
+}
 
+func Constructor() MedianFinder {
+	return MedianFinder{}
+}
+
+func (mf *MedianFinder) AddNum(num int) {
+	minQ, maxQ := &mf.queMin, &mf.queMax
+	if minQ.Len() == 0 || num <= -minQ.IntSlice[0] {
+		heap.Push(minQ, -num)
+		if maxQ.Len()+1 < minQ.Len() {
+			heap.Push(maxQ, -heap.Pop(minQ).(int))
+		}
+	} else {
+		heap.Push(maxQ, num)
+		if maxQ.Len() > minQ.Len() {
+			heap.Push(minQ, -heap.Pop(maxQ).(int))
+		}
+	}
+}
+
+func (mf *MedianFinder) FindMedian() float64 {
+	minQ, maxQ := mf.queMin, mf.queMax
+	if minQ.Len() > maxQ.Len() {
+		return float64(-minQ.IntSlice[0])
+	}
+	return float64(maxQ.IntSlice[0]-minQ.IntSlice[0]) / 2
+}
+
+type hp struct{ sort.IntSlice }
+
+func (h *hp) Push(v interface{}) { h.IntSlice = append(h.IntSlice, v.(int)) }
+func (h *hp) Pop() interface{} {
+	a := h.IntSlice
+	v := a[len(a)-1]
+	h.IntSlice = a[:len(a)-1]
+	return v
+}
